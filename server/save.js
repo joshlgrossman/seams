5b1ddc45de843da04a82988a7b83b22c7cc2941b
joshlgrossman <josh.l.grossman@gmail.com>
2017-07-16 13:25:37 -0500

    rename var for consistency

--- server/save.js
+++ server/save.js
@@ -1,15 +1,15 @@
 const db = require('./db');
 const alias = require('./alias');
 
-function save(location, data) {
-  const {url} = alias(location);
+function save(url, data) {
+  const aliased = alias(url).url;
   const $set = {};
   for(const key in data) $set[`content.${key}`] = data[key];
 
   return new Promise((resolve, reject) => {
 
     if(!db.connection) resolve({});
-    else db.connection.updateOne({url}, {$set}, (err, result) => {
+    else db.connection.updateOne({url: aliased}, {$set}, (err, result) => {
       if(err) reject(err);
       else resolve(result ? result.result : {});
     });
