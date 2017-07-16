cd0b0f124002995a213093f796811d4dc8c0366b
joshlgrossman <josh.l.grossman@gmail.com>
2017-07-16 13:19:26 -0500

    save changes to mongodb

--- server/save.js
+++ server/save.js
@@ -1,6 +1,21 @@
 const db = require('./db');
+const alias = require('./alias');
 
-function save(url, data) {
+function save(location, data) {
+  const {url} = alias(location);
+  const $set = {};
+  for(const key in data) $set[`content.${key}`] = data[key];
+
+  return new Promise((resolve, reject) => {
+
+    if(!db.connection) resolve({});
+    else db.connection.updateOne({url}, {$set}, (err, result) => {
+      if(err) reject(err);
+      else resolve(result ? result.result : {});
+    });
+
+  });
+
 }
 
 module.exports = save;
