f6a5405a2ef5954b4b40a9424ba73a462d49a82b
joshlgrossman <josh.l.grossman@gmail.com>
2017-07-19 15:55:02 -0500

    protected routes and admin routes

--- server/alias.js
+++ server/alias.js
@@ -1,21 +1,27 @@
 const fileNameRegExp = /\/[^\/]+$/g;
 const fileTypeRegExp = /\.[^\.]+$/g;
+const adminFileRegExp = new RegExp(`^/admin.*`);
+const protectedFileRegExp = new RegExp(`^/admin.ui.*`);
 
 function alias(url) {
+
   if(url === '/') url = '/index.html';
 
   let fileName = url.match(fileNameRegExp);
   let fileType = url.match(fileTypeRegExp);
+  let adminFile = adminFileRegExp.test(url);
+  let protectedFile = adminFile && protectedFileRegExp.test(url);
 
   if(!fileName) {
     url = false;
     fileType = false;
   } else if(!fileType) {
     url += '.html';
-    fileType = ['.html'];
+    fileType = '.html';
   } else fileType = fileType[0];
 
-  return {url, fileType};
+  return {url, fileType, adminFile, protectedFile};
+
 }
 
 module.exports = alias;
