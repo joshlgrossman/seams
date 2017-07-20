09c3719927d35d806ba8552e10c5cbf00613d636
joshlgrossman <josh.l.grossman@gmail.com>
2017-07-20 10:18:47 -0500

    post with ajax to stop redirect, add error msgs

--- client/src/admin.js
+++ client/src/admin.js
@@ -1,3 +1,41 @@
+const {$, http} = require('./util');
+
 window.addEventListener('load', () => {
+
+  const $errors = $('.errors')[0];
+  const $name = $('.username')[0];
+  const $password = $('.password')[0];
+  const $submit = $('.submit')[0];
+  let timeout;
+
+  function showErrors(err) {
+    $errors.innerHTML = err;
+    if($errors.className.indexOf('show') === -1)
+      $errors.className += ' show';
+    clearTimeout(timeout);
+    timeout = setTimeout(hideErrors, 5000);
+  }
+
+  function hideErrors() {
+    $errors.className = $errors.className.replace(' show', '');
+  }
+
+  $submit.addEventListener('click', evt => {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    const name = $name.value;
+    const password = $password.value;
+
+    if(name && password) {
+      http.post('/', {name, password}).then(res => {
+        if(res.err) showErrors(res.msg || 'Invalid credentials');
+        else window.location.reload();
+      });
+    } else {
+      showErrors('Please enter username and password');
+    }
+
+  });
 
 });
