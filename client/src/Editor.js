909aaa010c7f548e950bf66d600daf0c38aeef80
joshlgrossman <josh.l.grossman@gmail.com>
2017-07-29 13:56:23 -0500

    editor stale check

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -3,6 +3,7 @@ const {bind} = require('./util');
 class Editor {
 
   constructor(element, property) {
+    this.stale = false;
     this.element = element;
     this.property = property;
     this.binding = bind(element, property);
