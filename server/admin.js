91ec1ceeab9c5309155e24e18102849621a5aac4
joshlgrossman <josh.l.grossman@gmail.com>
2017-07-29 12:10:16 -0500

    add classname to body in seams admin mode

--- server/admin.js
+++ server/admin.js
@@ -2,6 +2,7 @@ function admin(url, $) {
   $('head')
     .prepend(`<link rel="stylesheet" href="admin.ui.css">`)
     .prepend(`<script src="admin.ui.min.js"></script>`);
+  $('body').addClass('seams-admin-ui');
 }
 
 module.exports = admin;
