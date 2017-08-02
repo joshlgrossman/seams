1980c03cb48c5621c9602e7f8812d84e71106fc5
joshlgrossman <josh.l.grossman@gmail.com>
2017-08-01 22:55:20 -0500

    move cookie parser to admin module

--- server/admin.js
+++ server/admin.js
@@ -1,8 +1,17 @@
-function admin(url, $) {
+function inject(url, $) {
   $('head')
     .prepend(`<link rel="stylesheet" href="/seams.ui.css">`)
     .prepend(`<script src="/seams.ui.min.js"></script>`);
   $('body').addClass('seams-admin-ui');
 }
 
-module.exports = admin;
+function cookie(str) {
+  if(str)
+    return str.replace(/(?:(?:^|.*;\s*)seams-jwt\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+  else return 'seams-jwt';
+}
+
+module.exports = {
+  cookie,
+  inject
+};
