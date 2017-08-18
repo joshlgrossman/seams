03e8a0493ba59d93032a4b6696b74ce38c342f1e
joshlgrossman <josh.l.grossman@gmail.com>
2017-08-18 16:44:10 -0500

    refined cache unit test

--- tests/server/cache.test.js
+++ tests/server/cache.test.js
@@ -3,18 +3,24 @@ const {expect} = require('chai');
 const cache = require('../../server/cache');
 
 describe('server/cache', () => {
-  const _cache = cache(100000);
-  const content = {
-    'test 1': {value: 'hello'},
-    'test 2': {value: 'world'}
-  };
+
+  let _cache, content;
+
+  before(() => {
+    _cache = cache(100000);
+    content = {
+      'test 1': {value: 'hello'},
+      'test 2': {value: 'world'}
+    };
+  });
 
   for(const key in content) {
     const {value} = content[key];
 
-    it(`${key} should not be cached`, () => {
+    it(`${key} should cache properly`, () => {
       expect(_cache(key)).to.not.be.ok;
       _cache(key, value);
+      expect(_cache(key)).to.equal(value);
     });
 
   }
