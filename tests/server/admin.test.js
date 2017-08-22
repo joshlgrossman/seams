ca5ae2d2f66fcfae9c713ab864d65b9cb0971cc5
joshlgrossman <josh.l.grossman@gmail.com>
2017-08-22 17:05:48 -0500

    test admin#inject

--- tests/server/admin.test.js
+++ tests/server/admin.test.js
@@ -1,10 +1,40 @@
-const {describe, it} = require('mocha');
+const {describe, it, beforeEach} = require('mocha');
 const {expect} = require('chai');
 const admin = require('../../server/admin');
 
 describe('server/admin', () => {
 
+  function stub() {
+    const stub = () => stub;
+    stub.prepend = () => {
+      stub.prepend.called = (stub.prepend.called|0) + 1;
+      return stub;
+    };
+    stub.addClass = () => {
+      stub.addClass.called = (stub.addClass.called|0) + 1;
+      return stub;
+    };
+    return stub;
+  }
+
+  describe('#inject', () => {
+
+    let $;
+
+    beforeEach(() => {
+      $ = stub();
+    });
+
+    it('should prepend scripts and add class', () => {
+      admin.inject('test', $);
+      expect($.prepend.called).to.equal(2);
+      expect($.addClass.called).to.equal(1);
+    });
+
+  });
+
   describe('#cookie', () => {
+
     it('should return "seams-jwt"', () => {
       expect(admin.cookie()).to.equal('seams-jwt');
     });
@@ -16,6 +46,7 @@ describe('server/admin', () => {
     it('should not be ok', () => {
       expect(admin.cookie('seams-jwt=')).to.not.be.ok;
     });
+
   });
 
 });
